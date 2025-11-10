85308daf1ad92783e493aa9c44be783f4e25e441
jesse sheehan <jessetsheehan@gmail.com>
2025-11-10 15:20:20 -0500

    fixing kaigoo notes

--- src/pages/DesignProcess.jsx
+++ src/pages/DesignProcess.jsx
@@ -27,7 +27,7 @@ export default function DesignProcess() {
                     ></iframe>
                 </div>
                 <p className="text-muted mt-2 small text-center" style={{ maxWidth: '800px' }}>
-                    Pre-alpha footage of a version of Kaigoo's Revenge used for testing
+                    Pre-alpha footage showing Stage 0 and Stage 1 evolutions of Kaigoo's Revenge
                 </p>
             </div>
             
@@ -35,22 +35,30 @@ export default function DesignProcess() {
                 <h3>Game Design Notes for Kaigoo's Revenge</h3>
                 <h6 className="text-muted mb-3">Unreleased</h6>
                 <p className="mb-4">
-                    Kaigoo's Revenge is a title made in collaboration with CrabOnFire, in which I am the lead designer and support programmer for this Unity game. Below are my first round of game design notes upon returning after taking a short hiatus while I compeleted my batch at Recurse Center in Fall 2025.
+                    Kaigoo's Revenge is a title made in collaboration with CrabOnFire, in which I am the lead designer and support programmer for this Unity game. Below are a small selection of my first round of game design notes upon returning after taking a hiatus while I compeleted my batch at Recurse Center in Fall 2025.
                 </p>
 
                 <ul className="list-unstyled">
 
                 
-                    <li className="mb-2"><strong>General notes</strong> </li>
-                    <li className="mb-2">• Stage Zero needs the most work - it's really hard!</li>
-                    <li className="mb-2">• Stage 0 was set to a speed of 35 - possibly by mistake, as stage 1 was set to 25.</li>
-                    <li className="mb-2">• I lowered the speed to 15 so I could play through the game</li>
-                    <li className="mb-2">• There's no time to learn how to play, it throws you into the thick of it so quickly.</li>
-                    <li className="mb-2">• Make Stage 0 easier</li>
-                    <li className="mb-2">• Lessen the risk of falling into pits - move platforms closer together - the single jump + dash isn't powerful enough to cover many of the distances between platforms currently.</li>
-                    <li className="mb-2">• The game reacts and expects the player to react too fast at the start</li>
-                    <li className="mb-2">• When the game starts before activating other spawners, let's immediately spawn a single long, flat platform (orbs optional) that lasts 3-5 seconds - give the players a chance to breath and if it's their first time playing, jump a little bit, use the dash, etc</li>
+                    <li className="mb-2"><strong>Stage 0 Notes</strong> </li>
+
+                    <li className="mb-2">Stage 0 needs the most work - it's really hard!</li>
+                    <li className="mb-2">• It was set to a speed of 35 - possibly by mistake, as Stage 1 was set to 25. I lowered the speed to 15 so I could play through the gam - I think that's what we should stick with</li>
+                    
+                    <li className="mb-2">• Lessen the risk of falling into pits - move platforms closer together - the single jump + dash is barely powerful enough to cover many of the distances between platforms currently.</li>
+
+                    <li className="mb-2">• Let's delay the additional platforms a little longer, so the player has more time to get ready and if it's their first time playing, jump a little bit, use the dash, etc. Feels like you're thrown into the thick of it too quickly</li>
+                    <li className="mb-2">• Delay the enemy spawner so it doesn't start right away.</li>
+                    <li className="mb-2">• In this first stage, we should focus on platforms that allow the player to run automatically so it's harder for them to get pushed off stage - it's a little tricky right now. It's even a little confusing, as originally, I thought you'd be able to run past the computers - I didn't know you had to jump up above them.</li>
+
+
+                    <li className="mb-2"><strong>Stage 1</strong></li>
+                    <li className="mb-2">• Stage 1's attack cooldown should be shorter, so you can do them pretty quickly right after each other, like only last as long as the animation, so you can dash and attack back to back.</li>
+                    <li className="mb-2">• As we play through this, we'll have to determine the best length of time for the player to play before evolutions take place. Seems to difficult to determine at this point.</li> 
+
                     <li className="mb-2"><strong>Death/Respawn:</strong> </li> 
+
                     <li className="mb-2">• Death/Respawn is so quick and the one sfx flash is more distracting than helpful.</li>
                     <li className="mb-2">• We should rework the death animation</li>
                     <li className="mb-2">• Give the player a moment to realize what they did wrong - think of how in a Mario game, when you die from an enemy, Mario looks at the screen for a second, and then he bounces up before falling all the way down. Maybe we do something like that?</li>
@@ -60,16 +68,21 @@ export default function DesignProcess() {
                     <li className="mb-2">• Camera should lower back to ground if the player was high up</li>
                     <li className="mb-2">• We should raise the player in from the bottom on a natural in-game platform, rather than making a platform appear that suddenly disappears. The platform should lower after 10 seconds, or 3 seconds, after the player has jumped off it (whichever comes first)</li>
                     <li className="mb-2">• Think of the platforms that bring a player into the screen in Smash Bros - they disappear once you jump off, and if you don't jump off, they disappear after x amount of seconds.</li>
+
                     <li className="mb-2"><strong>Evolution/scene change:</strong> </li> 
 
                     <li className="mb-2">• Evolution/scene change is instantaneous</li>
                     <li className="mb-2">• We should make the evolution more of a moment - would be fun to see a pokemon-like evolution where the slime evolves into the next form while the rest of the game freezes around the player</li>
                     <li className="mb-2">• During/immediately after evolution, the player should be invincible for a few moments</li>
+
                     <li className="mb-2"><strong>Transition to new background/platforms:</strong></li>
+
                     <li className="mb-2">• Backgrounds should fade/slide in so it's not like a light switch</li>
                     <li className="mb-2">• Platforms from previous stage should either disappear, or the character should not be pushed off screen by them (this is mostly an issue for Stage 1 to 2 evolution, since the final Kaigoo form cannot jump and the stage 1 platforms can't seem to be sliced)</li>
+
                     <li className="mb-2"><strong>Question/suggestion:</strong> </li>
-                    <li className="mb-2">• Maybe the camera zoom out so you see the full screen - I think most runners don't have much camera movement at all, maybe we should think about doing it that way. Maybe it shift it slightly when the player moved up to the higher spawner - but we should always be able to see the lower platforms.</li>
+
+                    <li className="mb-2">• I think the camera zoom out so you see the full screen - most runners don't have much camera movement at all, maybe we should think about doing it that way. If we could always see the lower platform, the game would be far less cruel and challenging, I think.</li>
                 </ul>
             </div>
             
